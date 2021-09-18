2ec54a1b9afe2270f0e930ed9e537074ab2cd350
williamtien23 <57851829+williamtien23@users.noreply.github.com>
2021-09-18 17:39:23 -0400

    Update bash scripts

--- project.sh
+++ project.sh
@@ -1,14 +1,9 @@
-#! /bin/sh
 
 top_module=top_core
-cpp_module=module_top_core
-
+cpp_module=module_compliance
+test_dir="\/home\/william\/riscv-arch-test\/work\/rv32i_m\/I\/"
+testcase="add-01"
 PATH=/opt/riscv/bin:$PATH
-
-if [ "$2" = "-target" ]; then
-	top_module="$3"
-	cpp_module="$4"
-fi
 
 if [ "$1" = "lint" ]; then
 	echo "Linting"
@@ -22,7 +17,14 @@ elif [ "$1" = "build" ]; then
 	riscv64-unknown-linux-gnu-objdump -D main.elf > dump.txt
 	riscv64-unknown-linux-gnu-objcopy -O binary --change-section-address .data=0xC8 --pad-to 0x190 main.elf test_prog.bin
 
-elif [ "$1" = "simulate" ]; then
+elif [ "$1" = "target" ]; then
+	echo "Setting target"
+	testcase="$2"
+	cd Testbenches
+	rm module_compliance.h
+	vim -c "s/^/#define testcase \"$testcase\"\r#define test_dir \"$test_dir\"/" -c "wq" module_compliance.h
+
+elif [ "$1" = "sim" ]; then
 	echo "Verilating"
 	verilator --trace --cc --exe -y Src_v Testbenches/$cpp_module.cpp --top-module "$top_module" "$top_module".v
 	cd obj_dir
@@ -32,7 +34,10 @@ elif [ "$1" = "simulate" ]; then
 	g++ -I ../obj_dir -I/usr/share/verilator/include "$cpp_module".cpp /usr/share/verilator/include/verilated.cpp /usr/share/verilator/include/verilated_vcd_c.cpp ../obj_dir/V"$top_module"__ALL.a -o "$top_module".out
 	echo "Simulating"
 	./"$top_module".out
-	gtkwave trace.vcd
+
+elif [ "$1" = "wave" ]; then
+	cd Testbenches
+	gtkwave trace.vcd	
 fi
 
 
---
+++ run_arch_test.sh
@@ -0,0 +1,22 @@
+arch_test_path="/home/william/riscv-arch-test"
+project_dir="/home/william/William/w1"
+echo "compliance compilation"
+PATH=/opt/riscv/bin:$PATH
+cd $arch_test_path
+make clean
+make compile
+cd work/rv32i_m/I
+for f in *.elf; do riscv64-unknown-elf-objdump -D $f > "${f%.*}.txt"; done
+for f in *.elf; do riscv64-unknown-elf-objcopy -O binary $f "${f%.*}.bin"; done
+for f in *.bin
+do
+  echo "Test for ${f%.*}"
+  cd $project_dir &>/dev/null
+  bash project.sh target "${f%.*}" &>/dev/null
+  rm obj_dir -r &>/dev/null
+  bash project.sh sim &>/dev/null
+  echo "${f%.*} signature generated"
+done
+cd $arch_test_path
+make verify
+echo "done compliance suite run"
